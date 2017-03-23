a9ee71e9164cdadb2ffec19baeb9ba0af0aa8183
Cheikh Ahmed Tidiane DIENG <tehneophyt@gmail.com>
2017-03-23 23:29:35 +0000

    Ajout d'indice sur la seconde enigme encore

--- recrutement/code.php
+++ recrutement/code.php
@@ -10,13 +10,13 @@ use Endroid\QrCode\QrCode;
 $qrCode = new QrCode();
 
 $qrCode
-    ->setText("Bienvenue BornToCode - Sup Info ! Si vous l'êtes dans l'âme, continuons ensemble sur https://github.com/BornToCode-SI/borntocode")
+    ->setText("Gpxqkgu wp ockn cxge Pqo, rtgpqo, vénérjqpg, gockn uwt uwrkphqdqtpvqeqfg@iockn.eqo")
     ->setSize(500)
     ->setPadding(10)
     ->setErrorCorrection('high')
     ->setForegroundColor(['r' => 1, 'g' => 99, 'b' => 174, 'a' => 0])
     ->setBackgroundColor(['r' => 255, 'g' => 255, 'b' => 255, 'a' => 0])
-    ->setLabel('Avez-vous cassé le code ?')
+    ->setLabel('Grand jules de Rome de ces deux doigt!')
     ->setLabelFontSize(16)
     ->setImageType(QrCode::IMAGE_TYPE_PNG)
 ;
@@ -24,4 +24,4 @@ $qrCode
 //header('Content-Type: '.$qrCode->getContentType());
 //$qrCode->render();
 
-$qrCode->save('code.png');
+$qrCode->save('code2.png');
